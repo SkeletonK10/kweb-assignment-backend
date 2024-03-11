9df705089c36d2b5aa867c5d24b987a255d036e5
skeletonk10 <poohbilly2@naver.com>
2024-03-11 20:17:52 +0900

    [Fix] Remove db connection log

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,6 @@ import { Client } from "pg";
 import { dbConfig } from "./secret";
 
 export const getConnection = async () => {
-  console.log("CONNECT TO DB");
   const client = new Client(dbConfig);
   await client.connect();
   return client;
